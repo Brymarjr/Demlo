efcfceca30817b9a7cc0907afd6a32b79f532ce3
Brymarjr <braimaholatilewa@gmail.com>
2026-06-09 03:49:57 +0100

    MIGRATION PL-21: Generate and execute script for refresh_tokens table schema

--- PeerLend.Infrastructure/Persistence/Migrations/20260609024734_AddRefreshTokensTable.Designer.cs
+++ PeerLend.Infrastructure/Persistence/Migrations/20260609024734_AddRefreshTokensTable.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PeerLend.Infrastructure.Persistence;
 
 #nullable disable
 
-namespace PeerLend.Infrastructure.Migrations
+namespace PeerLend.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(PeerLendDbContext))]
-    partial class PeerLendDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260609024734_AddRefreshTokensTable")]
+    partial class AddRefreshTokensTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
